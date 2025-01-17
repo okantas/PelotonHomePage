9b85bcbad7600e3a69aef15958d8912860d1a505
okantas <okantas954@gmail.com>
2025-01-18 00:39:23 +0300

    photos are implemented

---
+++ components/Header.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <main>
+      <div>
+        <h1>LOGO</h1>
+
+        <ul>
+          <li>Bikes</li>
+          <li>Tread</li>
+          <li>Strength</li>
+          <li>Row</li>
+          <li>App</li>
+          <li>Accessories</li>
+          <li>Apparel</li>
+        </ul>
+
+        <div></div>
+      </div>
+    </main>
+  );
+}
