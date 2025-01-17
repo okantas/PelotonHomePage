951b2c9ae4e97ec24b0625c8b2d8cae9bf592f9b
okantas <okantas954@gmail.com>
2025-01-18 01:12:14 +0300

    basic design is implemented on the Header

--- components/Header.js
+++ components/Header.js
@@ -1,22 +1,35 @@
 import React from "react";
+import Image from "next/image";
+import Logo from "../public/PelotonLogo.svg";
+import Location from "../public/locationLogo.svg";
+import ShoppingCart from "../public/ShoppingCartIcon.svg";
+import Human from "../public/humanIcon.svg";
 
 export default function Header() {
   return (
-    <main>
+    <main className="flex bg-slate-600 justify-between items-center py-2 px-8 max-w-full  ">
       <div>
-        <h1>LOGO</h1>
+        <Image src={Logo} alt="Logo" />
+      </div>
 
-        <ul>
-          <li>Bikes</li>
-          <li>Tread</li>
-          <li>Strength</li>
-          <li>Row</li>
-          <li>App</li>
-          <li>Accessories</li>
-          <li>Apparel</li>
-        </ul>
+      <ul className="flex justify-center gap-x-9 font-bold text-white text-xs">
+        <li>Bikes</li>
+        <li>Tread</li>
+        <li>Strength</li>
+        <li>Row</li>
+        <li>App</li>
+        <li>Accessories</li>
+        <li>Apparel</li>
+      </ul>
 
-        <div></div>
+      <div className="flex gap-6">
+        <Image src={Location} alt="location" className="cursor-pointer" />
+        <Image src={Human} alt="human" className="cursor-pointer" />
+        <Image
+          src={ShoppingCart}
+          alt="shoppingcart"
+          className="cursor-pointer"
+        />
       </div>
     </main>
   );
