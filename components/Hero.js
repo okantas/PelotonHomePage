60c7308a12a07c0cb26e18390bf24c8feec9a3b7
okantas <okantas954@gmail.com>
2025-01-19 23:18:19 +0300

    content is updated

--- components/Hero.js
+++ components/Hero.js
@@ -5,7 +5,16 @@ export default function HeroSection() {
   return (
     <main>
       <Header />
-      <div className="bg-[url('/HeroImg.svg')] bg-cover bg-center w-full h-screen "></div>
+      <div className="bg-[url('/HeroImg.svg')] bg-cover bg-center w-full h-screen shadow-[10px_10px_30px_rgba(0,0,0,1)] ">
+        <div>
+          <p>$150 off packages</p>
+          <h1>Spring training savings</h1>
+          <p>
+            Limited-time offer! Build strength this spring with a Peloton Guide
+            Package and get $150 off.**
+          </p>
+        </div>{" "}
+      </div>
     </main>
   );
 }
