b56a839a20bd850e9be3a44363a12d2dc7f91246
okantas <okantas954@gmail.com>
2025-01-20 23:47:49 +0300

    Hero Section is ready

--- components/Hero.js
+++ components/Hero.js
@@ -12,24 +12,26 @@ export default function HeroSection() {
           backgroundImage: "url('/HeroImg.svg')",
         }}
       >
-        <div className="absolute inset-0 bg-black opacity-50"></div>
-        <div className="absolute gap-6 flex flex-col h-screen items-start justify-center pl-20">
+        <div className="absolute inset-0 bg-black opacity-60"></div>
+        <div className="absolute gap-8 flex flex-col h-screen items-start justify-center pl-20">
           <p className="font-bold text-sm text-white tracking-widest">
             $150 OFF PACKAGES
           </p>
           <h1 className="font-bold text-5xl text-white tracking-widest">
             SPRING TRAINING SAVINGS
           </h1>
-          <p className="text-2xl text-white font-thin w-2/4">
+          <p className="text-2xl text-white font-thin w-2/4 tracking-widest">
             Limited-time offer! Build strength this spring with a Peloton Guide
             Package and get $150 off.**
           </p>
 
-          <a className="border border-solid border-red-600 px-16 py-3 bg-red-800 rounded-md text-white text-sm ">
+          <a className="border border-solid border-red-600 px-16 py-3 bg-red-800 rounded-md text-white text-sm tracking-widest">
             SHOP GUIDE
           </a>
 
-          <p>Peloton Guide Membership separate</p>
+          <p className="text-sm text-white font-extralight tracking-widest">
+            Peloton Guide Membership separate
+          </p>
         </div>
       </div>
     </main>
