b118ad4cd443c8d562b36b49ba03207d3c216252
okantas <okantas954@gmail.com>
2025-01-20 23:28:27 +0300

    shadow is fixed

--- components/Hero.js
+++ components/Hero.js
@@ -12,12 +12,13 @@ export default function HeroSection() {
           backgroundImage: "url('/HeroImg.svg')",
         }}
       >
-        <div className="absolute inset-0 bg-gradient-to-t via-transparent to-black opacity-70"></div>
-        <div className="absolute inset-0 bg-gradient-to-r via-transparent to-black opacity-50"></div>
+        <div className="absolute inset-0 bg-black opacity-50"></div>
         <div className="absolute gap-4 flex flex-col h-screen items-start justify-center pl-20">
-          <p className="font-bold text-sm text-white">$150 off packages</p>
-          <h1 className="font-bold text-5xl text-white">
-            Spring training savings
+          <p className="font-bold text-sm text-white tracking-widest">
+            $150 OFF PACKAGES
+          </p>
+          <h1 className="font-bold text-5xl text-white tracking-widest">
+            SPRING TRAINING SAVINGS
           </h1>
           <p className="text-2xl text-white font-thin w-2/4">
             Limited-time offer! Build strength this spring with a Peloton Guide
