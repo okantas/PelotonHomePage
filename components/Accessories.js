9bc7cfd595cf52580fa16d915cd7301448b082a2
okantas <okantas954@gmail.com>
2025-01-22 00:50:08 +0300

    all pictures are implemented

--- components/Accessories.js
+++ components/Accessories.js
@@ -1,9 +1,19 @@
 import React from "react";
+import Image from "next/image";
+import Row from "@/public/Row";
+import Tread from "@/public/Tread";
+import Bikes from "@/public/Bikes";
+import Guide from "@/public/Guide";
+import App from "@/public/App";
+import AccessoriesImg from "@/public/accessories";
+import Apparel from "@/public/Apparel";
 
 export default function Accessories() {
   return (
     <main>
-      <div></div>
+      <div>
+        <div></div>
+      </div>
     </main>
   );
 }
