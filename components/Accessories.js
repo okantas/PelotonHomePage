4ab9ac1ca3eff2b5f3a8d3fcd85ea408c36f9c40
okantas <okantas954@gmail.com>
2025-01-22 01:04:03 +0300

    first Image is implemented

--- components/Accessories.js
+++ components/Accessories.js
@@ -1,18 +1,21 @@
 import React from "react";
 import Image from "next/image";
-import Row from "@/public/Row";
-import Tread from "@/public/Tread";
-import Bikes from "@/public/Bikes";
-import Guide from "@/public/Guide";
-import App from "@/public/App";
-import AccessoriesImg from "@/public/accessories";
-import Apparel from "@/public/Apparel";
+import Row from "@/public/Row.svg";
+import Tread from "@/public/Tread.svg";
+import Bikes from "@/public/bikes.svg";
+import Guide from "@/public/Guide.svg";
+import AppImg from "@/public/app.svg";
+import AccessoriesImg from "@/public/accessories.svg";
+import Apparel from "@/public/Apparel.svg";
 
 export default function Accessories() {
   return (
     <main>
       <div>
-        <div></div>
+        <div className=" shadow-black shadow-2xl w-36 h-44">
+          <Image src={Row} alt="row" />
+          <p className="text-right">Row</p>
+        </div>
       </div>
     </main>
   );
