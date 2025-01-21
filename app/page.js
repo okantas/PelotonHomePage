fc50f56d456eede9f5854838e5d81ca1a9f98637
okantas <okantas954@gmail.com>
2025-01-22 00:52:01 +0300

    Accessories is implemented to the page file

--- app/page.js
+++ app/page.js
@@ -1,12 +1,13 @@
 import React from "react";
-import Header from "@/components/Header";
 import Hero from "@/components/Hero";
+import Accessories from "@/components/Accessories";
 
 export default function Home() {
   return (
     <>
       <div>
         <Hero />
+        <Accessories />
       </div>
     </>
   );
