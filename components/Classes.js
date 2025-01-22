094ae78edf7097ec5174bfd832d76c95e495efb7
okantas <okantas954@gmail.com>
2025-01-23 00:22:47 +0300

    script part is done

--- components/Classes.js
+++ components/Classes.js
@@ -3,7 +3,18 @@ import React from "react";
 export default function Classes() {
   return (
     <main>
-      <div></div>
+      <div className="flex flex-col items-center py-24">
+        <div className="w-1/2 flex flex-col gap-6 text-center">
+          <h1 className="text-5xl font-thin tracking-wides">
+            Tap into motivation and feel energized class after class
+          </h1>
+          <p className="text-xl font-thin tracking-wides">
+            Get a taste for workouts that fit your mood, goals, music style,
+            experience level, and schedule on the Peloton Bike, Bike+, Tread,
+            Guide, and the Peloton App.
+          </p>
+        </div>
+      </div>
     </main>
   );
 }
