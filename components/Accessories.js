cd746c6a45fca01f0d7c760dd0b34228b5a85eb6
okantas <okantas954@gmail.com>
2025-01-22 23:54:30 +0300

    Map version is generated

--- components/Accessories.js
+++ components/Accessories.js
@@ -9,55 +9,38 @@ import AccessoriesImg from "@/public/accessories.svg";
 import Apparel from "@/public/Apparel.svg";
 
 export default function Accessories() {
+  const Pictures = [
+    { id: 1, name: Row, alt: "row", text: "Row" },
+    { id: 2, name: Tread, alt: "Tread", text: "Tread" },
+    { id: 3, name: Bikes, alt: "Bikes", text: "Bikes" },
+    { id: 4, name: Guide, alt: "Guide", text: "Guide" },
+    { id: 5, name: AppImg, alt: "AppImg", text: "App" },
+    {
+      id: 6,
+      name: AccessoriesImg,
+      alt: "AccessoriesImg",
+      text: "Accessories",
+    },
+    { id: 7, name: Apparel, alt: "Apparel", text: "Apparel" },
+  ];
   return (
     <main>
-      <div className=" flex gap-10 py-14 px-20 w-full bg-[#F5F7F9]">
-        <div className=" shadow-black  w-36 h-44 rounded-md bg-white">
-          <Image src={Row} alt="row" className="relative  right-6" />
-          <p className="relative right-3 font-bold text-right text-slate-500">
-            Row
-          </p>
-        </div>
-        <div className=" shadow-black  w-36 h-44 rounded-md bg-white">
-          <Image src={Tread} alt="Tread" className="relative  right-6" />
-          <p className="relative right-3 font-bold text-right text-slate-500">
-            Row
-          </p>
-        </div>
-        <div className=" shadow-black  w-36 h-44 rounded-md bg-white">
-          <Image src={Bikes} alt="Bikes" className="relative  right-6" />
-          <p className="relative right-3 font-bold text-right text-slate-500">
-            Row
-          </p>
-        </div>
-        <div className=" shadow-black  w-36 h-44 rounded-md bg-white">
-          <Image src={Guide} alt="Guide" className="relative  right-6" />
-          <p className="relative right-3 font-bold text-right text-slate-500">
-            Row
-          </p>
-        </div>
-        <div className=" shadow-black  w-36 h-44 rounded-md bg-white">
-          <Image src={AppImg} alt="AppImg" className="relative  right-6" />
-          <p className="relative right-3 font-bold text-right text-slate-500">
-            Row
-          </p>
-        </div>
-        <div className=" shadow-black  w-36 h-44 rounded-md bg-white">
-          <Image
-            src={AccessoriesImg}
-            alt="AccessoriesImg"
-            className="relative  right-6"
-          />
-          <p className="relative right-3 font-bold text-right text-slate-500">
-            Row
-          </p>
-        </div>
-        <div className=" shadow-black  w-36 h-44 rounded-md bg-white">
-          <Image src={Apparel} alt="Apparel" className="relative  right-6" />
-          <p className="relative right-3 font-bold text-right text-slate-500">
-            Row
-          </p>
-        </div>
+      <div className="flex gap-10 py-14 px-20 w-full bg-[#F5F7F9]">
+        {Pictures.map((picture) => (
+          <div
+            className=" shadow-black  w-36 h-44 rounded-md bg-white"
+            key={picture.id}
+          >
+            <Image
+              src={picture.name}
+              alt={picture.alt}
+              className="relative  right-6"
+            />
+            <p className="relative right-3 font-bold text-right text-slate-500">
+              {picture.text}
+            </p>
+          </div>
+        ))}
       </div>
     </main>
   );
