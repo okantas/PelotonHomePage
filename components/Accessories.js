9298a2bec3a4ca709eff01cb92eb5d0ae825a594
okantas <okantas954@gmail.com>
2025-01-22 23:23:42 +0300

    other cards and pictures implemented

--- components/Accessories.js
+++ components/Accessories.js
@@ -11,9 +11,49 @@ import Apparel from "@/public/Apparel.svg";
 export default function Accessories() {
   return (
     <main>
-      <div className="py-14 px-20 w-full bg-[#F5F7F9]">
+      <div className=" flex gap-10 py-14 px-20 w-full bg-[#F5F7F9]">
         <div className=" shadow-black  w-36 h-44 rounded-md bg-white">
           <Image src={Row} alt="row" className="relative  right-6" />
+          <p className="relative right-3 font-bold text-right text-slate-500">
+            Row
+          </p>
+        </div>
+        <div className=" shadow-black  w-36 h-44 rounded-md bg-white">
+          <Image src={Tread} alt="Tread" className="relative  right-6" />
+          <p className="relative right-3 font-bold text-right text-slate-500">
+            Row
+          </p>
+        </div>
+        <div className=" shadow-black  w-36 h-44 rounded-md bg-white">
+          <Image src={Bikes} alt="Bikes" className="relative  right-6" />
+          <p className="relative right-3 font-bold text-right text-slate-500">
+            Row
+          </p>
+        </div>
+        <div className=" shadow-black  w-36 h-44 rounded-md bg-white">
+          <Image src={Guide} alt="Guide" className="relative  right-6" />
+          <p className="relative right-3 font-bold text-right text-slate-500">
+            Row
+          </p>
+        </div>
+        <div className=" shadow-black  w-36 h-44 rounded-md bg-white">
+          <Image src={AppImg} alt="AppImg" className="relative  right-6" />
+          <p className="relative right-3 font-bold text-right text-slate-500">
+            Row
+          </p>
+        </div>
+        <div className=" shadow-black  w-36 h-44 rounded-md bg-white">
+          <Image
+            src={AccessoriesImg}
+            alt="AccessoriesImg"
+            className="relative  right-6"
+          />
+          <p className="relative right-3 font-bold text-right text-slate-500">
+            Row
+          </p>
+        </div>
+        <div className=" shadow-black  w-36 h-44 rounded-md bg-white">
+          <Image src={Apparel} alt="Apparel" className="relative  right-6" />
           <p className="relative right-3 font-bold text-right text-slate-500">
             Row
           </p>
