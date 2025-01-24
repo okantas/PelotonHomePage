d131d7d86dd7405216edb3d541eec54ebbdaacfb
okantas <okantas954@gmail.com>
2025-01-24 23:53:26 +0300

    swiper component created

--- app/page.js
+++ app/page.js
@@ -2,6 +2,7 @@ import React from "react";
 import Hero from "@/components/Hero";
 import Accessories from "@/components/Accessories";
 import Classes from "@/components/Classes";
+import TrainingVideos from "@/components/TrainingVideos";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
         <Hero />
         <Accessories />
         <Classes />
+        <TrainingVideos />
       </div>
     </>
   );
---
+++ components/TrainingVideos.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function TrainingVideos() {
+  return (
+    <main>
+      <div></div>
+    </main>
+  );
+}
