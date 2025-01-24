872d0697f5832caee3f240d6f0e733e826eac94c
okantas <okantas954@gmail.com>
2025-01-25 02:18:15 +0300

    Swiper js is implemented

--- components/TrainingVideos.js
+++ components/TrainingVideos.js
@@ -1,4 +1,11 @@
+"use client";
 import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination } from "swiper/modules";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 import Image from "next/image";
 import Running from "@/public/Running.svg";
 import Strenght from "@/public/Strength.svg";
@@ -8,8 +15,45 @@ import Cycling from "@/public/Cycling.svg";
 
 export default function TrainingVideos() {
   return (
-    <main>
-      <div></div>
+    <main className="w-full flex justify-center items-cente">
+      <Swiper
+        modules={[Navigation, Pagination]}
+        navigation
+        pagination={{ clickable: true }}
+        spaceBetween={20}
+        slidesPerView={5}
+      >
+        <SwiperSlide>
+          <Image src={Running} width={280} height={200} alt="running" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image src={Boxing} width={280} height={200} alt="running" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image src={Strenght} width={280} height={200} alt="running" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image src={Rowing} width={280} height={200} alt="running" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image src={Cycling} width={280} height={200} alt="running" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image src={Running} width={280} height={200} alt="running" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image src={Boxing} width={280} height={200} alt="running" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image src={Strenght} width={280} height={200} alt="running" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image src={Rowing} width={280} height={200} alt="running" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image src={Cycling} width={280} height={200} alt="running" />
+        </SwiperSlide>
+      </Swiper>
     </main>
   );
 }
