6774c178faa416cb3bddd601d1ec947268d42ea2
okantas <okantas954@gmail.com>
2025-01-25 00:05:27 +0300

    svg images are implemented

--- components/TrainingVideos.js
+++ components/TrainingVideos.js
@@ -1,5 +1,10 @@
 import React from "react";
 import Image from "next/image";
+import Running from "@/public/Running.svg";
+import Strenght from "@/public/Strength.svg";
+import Rowing from "@/public/Rowing.svg";
+import Boxing from "@/public/Boxing.svg";
+import Cycling from "@/public/Cycling.svg";
 
 export default function TrainingVideos() {
   return (
