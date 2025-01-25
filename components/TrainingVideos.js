fe9ba1d8d2636491d0813e651b771b9bd3c5ec47
okantas <okantas954@gmail.com>
2025-01-25 03:38:36 +0300

    map function is implemented to the swiper js

--- components/TrainingVideos.js
+++ components/TrainingVideos.js
@@ -14,6 +14,14 @@ import Boxing from "@/public/Boxing.svg";
 import Cycling from "@/public/Cycling.svg";
 
 export default function TrainingVideos() {
+  const Photos = [
+    { id: 1, name: Running, alt: "Running" },
+    { id: 2, name: Strenght, alt: "Strenght" },
+    { id: 3, name: Rowing, alt: "Rowing" },
+    { id: 4, name: Boxing, alt: "Boxing" },
+    { id: 5, name: Cycling, alt: "Cycling" },
+    { id: 6, name: Boxing, alt: "Boxing" },
+  ];
   return (
     <main className="w-full flex justify-center items-cente">
       <Swiper
@@ -22,38 +30,14 @@ export default function TrainingVideos() {
         pagination={{ clickable: true }}
         spaceBetween={20}
         slidesPerView={5}
+        loop={true}
         className="mb-12"
       >
-        <SwiperSlide>
-          <Image src={Running} width={280} height={200} alt="running" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <Image src={Boxing} width={280} height={200} alt="running" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <Image src={Strenght} width={280} height={200} alt="running" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <Image src={Rowing} width={280} height={200} alt="running" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <Image src={Cycling} width={280} height={200} alt="running" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <Image src={Running} width={280} height={200} alt="running" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <Image src={Boxing} width={280} height={200} alt="running" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <Image src={Strenght} width={280} height={200} alt="running" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <Image src={Rowing} width={280} height={200} alt="running" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <Image src={Cycling} width={280} height={200} alt="running" />
-        </SwiperSlide>
+        {Photos.map((photo) => (
+          <SwiperSlide key={photo.id}>
+            <Image src={photo.name} width={280} height={200} alt={photo.alt} />
+          </SwiperSlide>
+        ))}
       </Swiper>
     </main>
   );
