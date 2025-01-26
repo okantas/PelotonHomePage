20a5b332361c966419d183638757ca3cb48f28fb
okantas <okantas954@gmail.com>
2025-01-26 05:45:08 +0300

    Image is implemented to the page

--- components/QuizSection.js
+++ components/QuizSection.js
@@ -1,9 +1,13 @@
 import React from "react";
+import Image from "next/image";
+import Desktop from "@/public/desktop.svg";
 
 export default function QuizSection() {
   return (
     <main>
-      <div></div>
+      <div>
+        <Image src={Desktop} alt="desktop" />
+      </div>
     </main>
   );
 }
