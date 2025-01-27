79d11b04249d7a1f0d217563359685222b2b3697
okantas <okantas954@gmail.com>
2025-01-27 19:05:29 +0300

    make changes on font and text

--- components/QuizSection.js
+++ components/QuizSection.js
@@ -7,9 +7,9 @@ export default function QuizSection() {
     <main>
       <div className="flex justify-center gap-40">
         <Image src={Desktop} alt="desktop" />
-        <div>
-          <p>Find your class match</p>
-          <h1>
+        <div className="flex flex-col gap-4">
+          <p className="font-bold text-xs">Find your class match</p>
+          <h1 className=" font-thin text-3xl">
             Get personalized class recommendations based on your fitness goals
           </h1>
           <a>Take Quiz</a>
