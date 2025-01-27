97a2417045b03c3779ac493661cd92d6228b0ad6
okantas <okantas954@gmail.com>
2025-01-27 19:00:49 +0300

    implemented the script section

--- components/QuizSection.js
+++ components/QuizSection.js
@@ -5,8 +5,15 @@ import Desktop from "@/public/desktop.svg";
 export default function QuizSection() {
   return (
     <main>
-      <div>
+      <div className="flex justify-center gap-40">
         <Image src={Desktop} alt="desktop" />
+        <div>
+          <p>Find your class match</p>
+          <h1>
+            Get personalized class recommendations based on your fitness goals
+          </h1>
+          <a>Take Quiz</a>
+        </div>
       </div>
     </main>
   );
