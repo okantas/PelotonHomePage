853f85dda7800a624f6cd9f7dd072f89cfd41ad3
okantas <okantas954@gmail.com>
2025-01-27 19:19:15 +0300

    button is implemented

--- components/QuizSection.js
+++ components/QuizSection.js
@@ -7,12 +7,14 @@ export default function QuizSection() {
     <main>
       <div className="flex justify-center gap-40">
         <Image src={Desktop} alt="desktop" />
-        <div className="flex flex-col gap-4">
+        <div className="flex flex-col gap-4 justify-center">
           <p className="font-bold text-xs">Find your class match</p>
           <h1 className=" font-thin text-3xl">
             Get personalized class recommendations based on your fitness goals
           </h1>
-          <a>Take Quiz</a>
+          <a className="font-bold text-cs border border-solid rounded-md border-black w-60 text-center p-2">
+            Take Quiz
+          </a>
         </div>
       </div>
     </main>
