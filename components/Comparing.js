e329fe13935b53dfef3c290220948b2fe647b5f2
okantas <okantas954@gmail.com>
2025-01-29 21:27:38 +0300

    Structure of the boxes is coded

--- components/Comparing.js
+++ components/Comparing.js
@@ -3,7 +3,13 @@ import React from "react";
 export default function Comparing() {
   return (
     <main>
-      <div></div>
+      <div>
+        <div>Fotoğraf</div>
+        <div>
+          <div>birinci kutu</div>
+          <div>İkinci kutu</div>
+        </div>
+      </div>
     </main>
   );
 }
