ff2485c3014ad72e1f72a9b254cec073f14afb53
okantas <okantas954@gmail.com>
2025-02-04 22:55:48 +0300

    image is implemented

--- components/Comparing.js
+++ components/Comparing.js
@@ -1,11 +1,14 @@
 import React from "react";
 import bikes from "@/public/bikes.svg";
+import Image from "next/image";
 
 export default function Comparing() {
   return (
     <main>
       <div>
-        <div>Fotoğraf</div>
+        <Image alt="bikes">
+          <bikes />
+        </Image>
         <div>
           <div>birinci kutu</div>
           <div>İkinci kutu</div>
