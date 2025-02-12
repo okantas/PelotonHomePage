6aa2e8a90fbc948101036ef2ac22a82a50cce1fc
okantas <okantas954@gmail.com>
2025-02-12 22:14:07 +0300

    shadow is implemented

--- components/Comparing.js
+++ components/Comparing.js
@@ -6,13 +6,15 @@ export default function Comparing() {
   return (
     <main>
       <div>
-        <div className="relative w-4/12 h-60">
+        <div className="relative w-1/4 ">
+          <div className="absolute inset-0 top-56 h-2/3 bg-gradient-to-t from-black to-transparent"></div>
           <Image
             src={bikes}
             alt="bikes"
-            className="w-full h-screen object-cover"
+            className=" h-auto w-3/7"
+            width={600}
+            height={800}
           />
-          <div className="absolute bg-black w-full h-screen"></div>
         </div>
 
         <div>
