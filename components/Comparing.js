4fc1919b8be1d12bd32d6ba7f8ff406a33b374ad
okantas <okantas954@gmail.com>
2025-02-12 22:24:55 +0300

    changed the width with px

--- components/Comparing.js
+++ components/Comparing.js
@@ -6,19 +6,19 @@ export default function Comparing() {
   return (
     <main>
       <div className="flex justify-center">
-        <div className="relative w-1/4 ">
-          <div className="absolute inset-0 top-60 h-2/3 bg-gradient-to-t from-black to-transparent"></div>
+        <div className="relative w-[600px] ">
+          <div className="absolute inset-0 top-[300px] h-2/3 bg-gradient-to-t from-black to-transparent"></div>
           <Image
             src={bikes}
             alt="bikes"
-            className=" h-auto w-3/7"
+            className=" h-auto w-[600px]"
             width={600}
             height={800}
           />
         </div>
 
         <div>
-          <div>birinci kutu</div>
+          <div className="relative  bg-black">birinci kutu</div>
           <div>İkinci kutu</div>
         </div>
       </div>
