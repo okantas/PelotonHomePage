906b15ac09f55a9f0301d74531fca0e17650f55d
okantas <okantas954@gmail.com>
2025-02-16 19:19:02 +0300

    bg image and sahow implemented , side box is desaigned

--- components/Items/items.js
+++ components/Items/items.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Bike from "./bike";
+import Others from "./others";
 
 export default function items() {
   return (
     <>
-      <div className="bg-[#F5F7F9] py-28">
+      <div className="flex flex-col gap-40 bg-[#F5F7F9] py-28 ">
         <Bike />
+        <Others />
       </div>
     </>
   );
--- components/Items/others.js
+++ components/Items/others.js
@@ -3,7 +3,14 @@ import React from "react";
 export default function others() {
   return (
     <>
-      <div></div>
+      <div className="flex justify-center gap-[20px]">
+        <div className="bg-[url(@/public/pelotonTreadBg.svg)] h-[480px] w-[580px]">
+          <div className="relative inset-0 h-[480px]  bg-gradient-to-t from-black to-transparent"></div>
+        </div>
+        <div className="bg-[#FFFFFF] shadow-[0px_32px_54px_-20px_rgba(0,0,0,0.1)] h-[480px] w-[580px]">
+          informations
+        </div>
+      </div>
     </>
   );
 }
