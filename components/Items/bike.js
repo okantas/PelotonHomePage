ec7759dd9c13a061151bcd40e2f6cf2570781822
okantas <okantas954@gmail.com>
2025-02-16 17:22:36 +0300

    implement the peloton bike boxes

--- components/Items/bike.js
+++ components/Items/bike.js
@@ -1,4 +1,7 @@
 import React from "react";
+import Image from "next/image";
+import PelotonBikePlus from "@/public/pelotonBike+.svg";
+import PelotonBike from "@/public/pelotonBike.svg";
 
 export default function Comparing() {
   return (
@@ -25,11 +28,57 @@ export default function Comparing() {
         </div>
 
         <div className="flex flex-col gap-[20px] ">
-          <div className=" bg-[#FFFFFF] relative w-[600px] h-[440px] shadow-[0_32px_52px_-20px_rgba(0,0,0,0.3)] rounded-md">
-            birinci kutu
+          <div className="flex justify-center items-center bg-[#FFFFFF] relative w-[580px] h-[440px] shadow-[0_32px_52px_-20px_rgba(0,0,0,0.1)] rounded-md ">
+            <p className="absolute top-6 left-8 text-[10px] text-[#FFFFFF] font-bold bg-[#D00C2A] px-1 py-[1px] rounded-[4px]">
+              SAVE ON PACKAGES
+            </p>
+            <div className="w-1/2 flex flex-col justify-center items-center">
+              <Image src={PelotonBikePlus} alt="Peloton Bike Plus" />
+            </div>
+            <div className="w-1/2 flex flex-col gap-2">
+              <h1 className="text-[28px] pb-3 ">Peloton Bike+</h1>
+              <p className="text-[12px] text-[#65666A]">Starting at</p>
+              <p className="text-[13px] font-bold">$2,495</p>
+              <p className="text-[13px] text-[#65666A]">
+                Pay monthly. Finance starting at 0% APR¹.
+                <br /> <br />
+                Peloton All-Access Membership ($44/mo) required to access
+                Peloton content
+              </p>
+              <a
+                href="https://www.google.com"
+                target="_blank"
+                className="text-[13px] font-bold pt-3 border-b border-black w-max"
+              >
+                EXPLORE BIKE+
+              </a>
+            </div>
           </div>
-          <div className=" bg-[#FFFFFF] relative w-[600px] h-[440px] shadow-[0_32px_52px_-20px_rgba(0,0,0,0.3)] rounded-md">
-            İkinci kutu
+          <div className="flex justify-center items-center bg-[#FFFFFF] relative w-[580px] h-[440px] shadow-[0_32px_52px_-20px_rgba(0,0,0,0.1)] rounded-md ">
+            <p className="absolute top-6 left-8 text-[10px] text-[#FFFFFF] font-bold bg-[#D00C2A] px-1 py-[1px] rounded-[4px]">
+              SAVE ON PACKAGES
+            </p>
+            <div className="w-1/2 flex flex-col justify-center items-center">
+              <Image src={PelotonBike} alt="Peloton Bike" />
+            </div>
+            <div className="w-1/2 flex flex-col gap-2">
+              <h1 className="text-[28px] pb-3 ">Peloton Bike</h1>
+              <p className="text-[12px] text-[#65666A]">Starting at</p>
+              <p className="text-[13px] font-bold">$1,445</p>
+              <p className="text-[13px] text-[#65666A]">
+                Pay monthly. Finance starting at 0% APR¹.
+                <br /> <br />
+                Peloton All-Access Membership ($44/mo) required to access
+                Peloton content
+              </p>
+              <a
+                href="https://www.google.com"
+                target="_blank"
+                className="text-[13px] font-bold pt-3 border-b border-black w-max"
+              >
+                EXPLORE BIKE
+              </a>
+            </div>
           </div>
         </div>
       </div>
