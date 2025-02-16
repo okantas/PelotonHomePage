14981dae7235a16a8d113013beeeeaff1222f6a9
okantas <okantas954@gmail.com>
2025-02-16 21:52:05 +0300

    first key of map is implemented

--- components/Items/others.js
+++ components/Items/others.js
@@ -1,54 +1,81 @@
 import React from "react";
 import Image from "next/image";
+import App from "@/public/pelotonApp.svg";
+import AppBg from "@/public/pelotonAppBg.svg";
+import Guide from "@/public/pelotonGuide.svg";
+import GuideBg from "@/public/pelotonGuideBg.svg";
+import Row from "@/public/pelotonRow.svg";
+import RowBg from "@/public/pelotonRowBg.svg";
+import Tread from "@/public/pelotonTread.svg";
+import TreadBg from "@/public/pelotonTreadBg.svg";
+import { Princess_Sofia } from "next/font/google";
 
 export default function others() {
+  const Items = [
+    {
+      id: 1,
+      leftName: "PELOTON TREAD",
+      message: "Feel inspired to go beyond the expected",
+      leftPhoto: TreadBg,
+      leftButton: "SHOP NOW",
+      rightName: "Peloton Tread",
+      price: "$3,495",
+      monthly:
+        "Peloton All-Access Membership ($44/mo) required to access Peloton content.",
+      rightButton: "Explore Tread",
+      rightPhoto: Tread,
+    },
+  ];
+
   return (
     <>
-      <div className="flex justify-center gap-[20px]">
-        <div className="bg-[url(@/public/pelotonTreadBg.svg)] h-[480px] w-[580px]">
-          <div className="relative inset-0  h-[480px]  bg-gradient-to-t from-black to-transparent">
-            <div className="flex flex-col justify-end h-[480px] gap-4 text-white pl-8 pb-10">
-              <p className="font-bold  ">PELOTON BIKE AND BIKE+</p>
-              <h1 className="font-thin text-[36px] ">
-                Switch up your routine <br /> with a variety of workouts
-              </h1>
+      {Items.map((item) => (
+        <div key={item.id} className="flex justify-center gap-[20px]">
+          <div
+            style={{ backgroundImage: `url(${item.leftPhoto.src})` }}
+            className=" h-[480px] w-[580px]"
+          >
+            <div className="relative inset-0  h-[480px]  bg-gradient-to-t from-black to-transparent">
+              <div className="flex flex-col justify-end h-[480px] gap-4 text-white pl-8 pb-10">
+                <p className="font-bold  ">{item.leftName}</p>
+                <h1 className="font-thin text-[36px] w-2/3 ">{item.message}</h1>
+                <a
+                  href="https://www.google.com/"
+                  target="_blank"
+                  className="font-bold border border-white border-solid w-60 py-[10px] text-center rounded-md "
+                >
+                  {item.leftButton}
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="flex justify-center items-center bg-[#FFFFFF] relative w-[580px] h-[480px] shadow-[0_32px_52px_-20px_rgba(0,0,0,0.1)] rounded-md ">
+            <p className="absolute top-6 left-8 text-[10px] text-[#FFFFFF] font-bold bg-[#D00C2A] px-1 py-[1px] rounded-[4px]">
+              SAVE ON PACKAGES
+            </p>
+            <div className="w-1/2 flex flex-col justify-center items-center">
+              <Image src={item.rightPhoto} alt="Peloton Bike Plus" />
+            </div>
+            <div className="w-1/2 flex flex-col gap-2">
+              <h1 className="text-[28px] pb-3 ">{item.rightName}</h1>
+              <p className="text-[12px] text-[#65666A]">Starting at</p>
+              <p className="text-[13px] font-bold">{item.price}</p>
+              <p className="text-[13px] text-[#65666A]">
+                Pay monthly. Finance starting at 0% APR¹.
+                <br /> <br />
+                {item.monthly}
+              </p>
               <a
-                href="https://www.google.com/"
+                href="https://www.google.com"
                 target="_blank"
-                className="font-bold border border-white border-solid w-60 py-[10px] text-center rounded-md "
+                className="text-[13px] font-bold pt-3 border-b border-black w-max"
               >
-                COMPARE BIKES
+                {item.rightButton}
               </a>
             </div>
           </div>
         </div>
-        <div className="flex justify-center items-center bg-[#FFFFFF] relative w-[580px] h-[480px] shadow-[0_32px_52px_-20px_rgba(0,0,0,0.1)] rounded-md ">
-          <p className="absolute top-6 left-8 text-[10px] text-[#FFFFFF] font-bold bg-[#D00C2A] px-1 py-[1px] rounded-[4px]">
-            SAVE ON PACKAGES
-          </p>
-          <div className="w-1/2 flex flex-col justify-center items-center">
-            <Image src={null} alt="Peloton Bike Plus" />
-          </div>
-          <div className="w-1/2 flex flex-col gap-2">
-            <h1 className="text-[28px] pb-3 ">arabam</h1>
-            <p className="text-[12px] text-[#65666A]">Starting at</p>
-            <p className="text-[13px] font-bold">param</p>
-            <p className="text-[13px] text-[#65666A]">
-              Pay monthly. Finance starting at 0% APR¹.
-              <br /> <br />
-              Peloton All-Access Membership ($44/mo) required to access Peloton
-              content
-            </p>
-            <a
-              href="https://www.google.com"
-              target="_blank"
-              className="text-[13px] font-bold pt-3 border-b border-black w-max"
-            >
-              butonum
-            </a>
-          </div>
-        </div>
-      </div>
+      ))}
     </>
   );
 }
