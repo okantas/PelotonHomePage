f36060d02410dc79a5ac233fa5622ada46acae14
okantas <okantas954@gmail.com>
2025-02-18 22:15:10 +0300

    implement the footer links

---
+++ components/Footer.js
@@ -0,0 +1,54 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <>
+      <div>
+        <div>
+          <div>
+            <div>
+              <p>Learn</p>
+              <a>Home Trial</a>
+              <a>Membership</a>
+              <a>Refurbished Bikes</a>
+              <a>Financing</a>
+              <a>Instructors</a>
+              <a>Corporate Wellnes</a>
+            </div>
+            <div>
+              <p>About</p>
+              <a>Our Story</a>
+              <a>Team</a>
+              <a>Careers</a>
+              <a>Press</a>
+              <a>Global</a>
+              <a>Blog</a>
+              <a>Investors</a>
+              <a>Our Pledge</a>
+            </div>
+            <div>
+              <a>Visit Us</a>
+              <a>Showrooms</a>
+              <a>Commercial Bikes</a>
+              <a>Hotel Finder</a>
+              <a>Book a Test Class</a>
+              <a>Studio</a>ß
+            </div>
+            <div>
+              <p>Support</p>
+              <a>Contact Peloton</a>
+              <a>Member Support Center</a>
+              <a>Return Policy</a>
+              <a>Warranties & Protection plans</a>
+              <a>Shipping</a>
+              <a>Product Recalls</a>
+              <a>Security</a>
+            </div>
+          </div>
+          <div></div>
+        </div>
+        <div></div>
+      </div>
+    </>
+  );
+}
