7ed7ae8c0121df3f5d80059df4c4b9795364410d
okantas <okantas954@gmail.com>
2025-02-18 21:24:53 +0300

    import the based part and also the image

--- components/TryApp.js
+++ components/TryApp.js
@@ -1,9 +1,25 @@
 import React from "react";
+import Image from "next/image";
+import TryTheApp from "@/public/TryTheApp.svg";
 
 export default function TryApp() {
   return (
     <>
-      <div></div>
+      <div>
+        <div>
+          <div>
+            <p>Peloton App</p>
+            <p>Try free for 30 days</p>
+            <p>
+              Take classes at home, in the gym, or on the go with no equipment
+              needed.
+            </p>
+            <a>TRY THE APP</a>
+            <p>New App trials only. $12.99/mo after. Terms apply.</p>
+          </div>
+          <Image src={TryTheApp} alt="image" />
+        </div>
+      </div>
     </>
   );
 }
