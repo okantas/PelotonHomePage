e1ff9a30b086d8c88bd8926a343a46ef3d25255b
okantas <okantas954@gmail.com>
2025-02-18 21:37:48 +0300

    made the design and update the layout

--- components/TryApp.js
+++ components/TryApp.js
@@ -6,18 +6,26 @@ export default function TryApp() {
   return (
     <>
       <div>
-        <div>
-          <div>
-            <p>Peloton App</p>
-            <p>Try free for 30 days</p>
-            <p>
-              Take classes at home, in the gym, or on the go with no equipment
-              needed.
+        <div className="flex items-center justify-center gap-40 pt-[120px]">
+          <div className="flex flex-col gap-[15px]">
+            <p className="text-[12px] text-[#181A1D] font-bold">Peloton App</p>
+            <p className="text-[28px] text-[#181A1D] font-light">
+              Try free for 30 days
             </p>
-            <a>TRY THE APP</a>
-            <p>New App trials only. $12.99/mo after. Terms apply.</p>
+            <p className="font-light text-[16px] text-[#65666A]">
+              Take classes at home, in the gym, or on the <br /> go with no
+              equipment needed.
+            </p>
+            <a className="w-60 h-10 bg-[#DF1C2F] justify-center items-center flex text-[#FFFFFF] font-bold text-[11px] rounded-md">
+              TRY THE APP
+            </a>
+            <p className="font-light text-[13px] text-[#65666A]">
+              New App trials only. $12.99/mo after. Terms apply.
+            </p>
           </div>
-          <Image src={TryTheApp} alt="image" />
+          <div className="w-[600px] h-[600px]">
+            <Image src={TryTheApp} alt="image" />
+          </div>
         </div>
       </div>
     </>
