416298341b12f3f28ba6a5129417d5f29effdbac
okantas <okantas954@gmail.com>
2025-08-02 01:57:53 +0300

    revise "Classes"

--- components/Classes.js
+++ components/Classes.js
@@ -5,16 +5,16 @@ export default function Classes() {
     <main>
       <div className="flex flex-col items-center py-24">
         <div className="w-1/2 flex flex-col gap-6 text-center">
-          <h1 className="text-5xl font-thin tracking-wides">
+          <h1 className="text-2xl lg:text-5xl font-thin tracking-wides">
             Tap into motivation and feel energized class after class
           </h1>
-          <p className="text-xl font-thin tracking-wides">
+          <p className="text-sm lg:text-xl font-thin tracking-wides">
             Get a taste for workouts that fit your mood, goals, music style,
             experience level, and schedule on the Peloton Bike, Bike+, Tread,
             Guide, and the Peloton App.
           </p>
         </div>
-        <div className="flex gap-4 mt-10">
+        <div className="flex flex-col md:flex-row gap-4 mt-10">
           <a className="flex bg-[#DF1C2F] border-2 border-solid border-[#DF1C2F]  text-xs justify-center items-center text-white w-60 h-10 rounded-md cursor-pointer tracking-widest">
             EXPLORE CLASSES
           </a>
